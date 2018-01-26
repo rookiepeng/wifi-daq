190de85e90d172cfac8fc492a6be1a265e4354ca
rookiepeng <rookie.dev@gmail.com>
2018-01-25 21:13:37 -0600

    add license declaration

--- CC3200/simplelink_conf.c
+++ CC3200/simplelink_conf.c
@@ -1,6 +1,21 @@
-/****************************************************************************************
-//    Copyright (C) 2017  Zhengyu Peng, zhengyu.peng@ttu.edu
-*****************************************************************************************/
+/*
+ *   simplelink_conf.c: SimpleLink confguration
+ * 
+ *   Copyright (C) 2017  Zhengyu Peng, https://zpeng.me
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -263,7 +278,7 @@ void SimpleLinkNetAppEventHandler(SlNetAppEvent_t *pNetAppEvent)
                    SL_IPV4_BYTE(g_ulStaIp, 1), SL_IPV4_BYTE(g_ulStaIp, 0));
 #endif
 
-        UpdateUDPClientIP(g_ulStaIp,1234);
+        UpdateUDPClientIP(g_ulStaIp, 1234);
 
         //Speaker Detected - Add Client
         //        g_UdpSock.Client.sin_family = AF_INET;
